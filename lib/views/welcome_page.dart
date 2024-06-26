c885decbcfce5a10c88953b47da0ecb010fae4b9
Akshay Kalwar <akshayjaiswal1618@gmail.com>
2024-06-27 00:53:52 +0530

    Welcome Screen done

--- lib/views/welcome_page.dart
+++ lib/views/welcome_page.dart
@@ -1,3 +1,4 @@
+import 'package:book_store/ReusebleWidgets/primaryButton.dart';
 import 'package:book_store/static/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -10,20 +11,69 @@ class HomePage extends StatelessWidget {
     final width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Container(
-            height: height * 0.70,
+            height: height * 0.60,
             width: width,
             color: primaryColor,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset('Assets/Images/book.png',height: 230,),
-                
-                Text("data")
-              ],
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('Assets/Images/book.png',height: 200,width: 370,),
+                  SizedBox(
+                    height: height * 0.01,
+                  ),
+                  Text("E - Book Store",style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                    color: Theme.of(context).colorScheme.background,
+                  ),),
+                  SizedBox(
+                    height: height * 0.01,
+                  ),
+                  Text("Here you can find best book for you and you can also read book and listens book",style: Theme.of(context).textTheme.labelSmall?.copyWith(
+                    color: Theme.of(context).colorScheme.background,
+                  ),
+                    textAlign: TextAlign.center,
+                  )
+                ],
+              ),
             ),
-          )
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+
+            children: [
+
+              SizedBox(
+                height: 30,
+              ),
+              Text("Disclaimer",style: Theme.of(context).textTheme.labelMedium
+                // ?.copyWith(color: Theme.of(context).colorScheme.background
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Text("n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum",
+                  style: Theme.of(context).textTheme.labelSmall,
+                  textAlign: TextAlign.center,
+                ),
+              )
+            ],
+          ),
+          Spacer(),
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: PrimaryButton(btnName: 'Continue', ontap: () {},
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
         ],
       ),
     );
